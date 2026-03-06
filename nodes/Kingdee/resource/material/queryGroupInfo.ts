1a48df6cf997f9588a29303669347729daee7b32
luka-mimi <luka.cat.mimi@gmail.com>
2026-03-06 09:23:54 +0800

    fix: 修复物料的分组信息查询的问题

--- nodes/Kingdee/resource/material/queryGroupInfo.ts
+++ nodes/Kingdee/resource/material/queryGroupInfo.ts
@@ -13,75 +13,49 @@ const operation: ResourceOperations = {
 	order: 16,
 	options: [
 		{
-			displayName: '分组字段标识',
+			displayName: '分组字段Key',
 			name: 'groupFieldKey',
 			type: 'string',
 			default: '',
-			required: true,
-			// description: '分组字段标识',
+			description: '不填时取默认，无默认，取第一个分组',
 		} as INodeProperties,
 		{
-			displayName: '查询方式',
-			name: 'queryType',
-			type: 'options',
-			default: 'id',
-			options: [
-				{
-					name: '按内码查询',
-					value: 'id',
-					description: '使用内码列表查询分组信息',
-				},
-				{
-					name: '按过滤条件查询',
-					value: 'filter',
-					description: '使用过滤条件查询分组信息',
-				},
-			],
-			description: '选择查询分组信息的方式',
+			displayName: '分组内码',
+			name: 'groupPkIds',
+			type: 'string',
+			default: '',
+			description: '格式："Id1,Id2,..."（使用分组内码时必录，分组内码和单据内码同时录时，分组内码优先）',
 		} as INodeProperties,
 		{
-			displayName: '内码列表',
+			displayName: '单据内码集合',
 			name: 'ids',
 			type: 'string',
 			default: '',
-			required: true,
-			displayOptions: {
-				show: {
-					queryType: ['id'],
-				},
-			},
-			description: '内码列表，多个内码用逗号分隔',
-		} as INodeProperties,
-		{
-			displayName: '过滤条件',
-			name: 'filterString',
-			type: 'string',
-			default: '',
-			displayOptions: {
-				show: {
-					queryType: ['filter'],
-				},
-			},
-			description: '过滤条件，支持 SQL 语法',
+			description: '格式："Id1,Id2,..."（使用内码时必录）',
 		} as INodeProperties,
 		commonOptions,
 	],
 	call: async function (index) {
-		const groupFieldKey = this.getNodeParameter('groupFieldKey', index) as string;
-		const queryType = this.getNodeParameter('queryType', index) as string;
+		const groupFieldKey = this.getNodeParameter('groupFieldKey', index, '') as string;
+		const groupPkIds = this.getNodeParameter('groupPkIds', index, '') as string;
+		const ids = this.getNodeParameter('ids', index, '') as string;
 
 		const data: Record<string, any> = {
-			GroupFieldKey: groupFieldKey,
+			FormId: FORM_ID,
 		};
 
-		if (queryType === 'id') {
-			const idsStr = this.getNodeParameter('ids', index) as string;
-			data.Ids = idsStr.split(',').map((n) => n.trim()).filter((n) => n);
-		} else {
-			data.FilterString = this.getNodeParameter('filterString', index, '') as string;
+		if (groupFieldKey) {
+			data.GroupFieldKey = groupFieldKey;
+		}
+		if (groupPkIds) {
+			data.GroupPkIds = groupPkIds;
+		}
+		if (ids) {
+			data.Ids = ids;
 		}
 
-		return RequestUtils.call.call(this, 'DynamicFormService', 'QueryGroupInfo', [FORM_ID, data]);
+		const result = await RequestUtils.call.call(this, 'DynamicFormService', 'QueryGroupInfo', [JSON.stringify(data)]);
+		return result?.NeedReturnData ?? result
 	},
 };
 
